e438c6f725f9f5e0a41a0a14983884ec512007a4
Aleksey Semenov <7.on.off@bk.ru>
2020-05-04 12:43:16 +0300

    add setyp.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='toml_config',
+    version='0.10',
+    packages=['toml_config'],
+    url='https://github.com/SemenovAV/toml_config',
+    license='MIT',
+    author='SemenovAV',
+    author_email='7.on.off@bk.ru',
+    description='Python library that simplifies parsing and creating Toml configuration files.'
+)
