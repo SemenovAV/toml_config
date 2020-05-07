b62f0be0d3cb963b9da560996a04610dbd7a57d0
Aleksey Semenov <7.on.off@bk.ru>
2020-05-07 12:00:14 +0300

    Update README.md and setyp.py

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # toml_config
 Python library that simplifies parsing and creating Toml configuration files.
 Wrapper on [toml](https://github.com/uiri/toml)
+
+## installation
+
+To install a module run the command:
+
+```pip install toml_config```
+
+## Using
+
+To get started, import the module. Create an instance of the Config class
+by passing as a parameter the path to an existing file or the path where this file
+will be created.And then you can read parameters from the file and write parameters to the file.
+
+Example.
+Creating a file and writing values.
+
+```main.py```:
+```python
+from toml_config.core import Config
+
+my_config = Config('app.config.toml')
+my_config.add_section('app').set(key='value',other_key=[1,2,3])
+```
+
+
+
+```app.config.toml```:
+```toml
+[app]
+key = "value"
+other_key = [ 1, 2, 3,]
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='toml_config',
-    version='0.12',
+    version='0.1.2',
     packages=['toml_config'],
     install_requires=['toml',],
     url='https://github.com/SemenovAV/toml_config',
     license='MIT',
     author='SemenovAV',
     author_email='7.on.off@bk.ru',
-    description='Python library that simplifies parsing and creating Toml configuration files.'
+    description='Python library that simplifies parsing and creating Toml configuration files.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
